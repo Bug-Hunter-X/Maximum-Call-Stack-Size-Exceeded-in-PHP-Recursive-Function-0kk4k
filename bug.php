01679405d378b9186b352c8153dbba8af8afa023
BugHunter-X <askzenai@gmail.com>
2024-12-12 22:08:04 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,11 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  } else if (a > b) {
+    return false; 
+  } else {
+    return foo(a + 1, b);
+  }
+}
+
+console.log(foo(5,10)); // this will result in Maximum call stack size exceeded
